02db295ee37db9d5214cec073dd22fa927495cab
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-04 23:50:41 +0600

    fetch api and get the path

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@ const searchPlayer = () => {
     const inputField = document.getElementById('input-field').value;
     console.log(inputField);
 
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputField}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.player[0]))
+
 }
